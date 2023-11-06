3e184752b73c9dd927c4c5d9359d8c033ea5bb1e
phamtanphat <phatdroid94@gmail.com>
2023-11-06 19:09:34 +0700

    findviewby

--- app/src/main/java/com/example/fragment19052023/MainActivity.kt
+++ app/src/main/java/com/example/fragment19052023/MainActivity.kt
@@ -2,16 +2,31 @@ package com.example.fragment19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnAddAndroid: Button
+    private lateinit var btnAddIOS: Button
+    private lateinit var btnReplaceAndroid: Button
+    private lateinit var btnReplaceIOS: Button
+    private lateinit var btnRemoveAndroid: Button
+    private lateinit var btnRemoveIOS: Button
+    private lateinit var btnPopBackStack: Button
+    private lateinit var btnDetach: Button
+    private lateinit var btnAttach: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        val androidFragment = AndroidFragment()
-        fragmentTransaction.add(R.id.linear_layout_container, androidFragment)
-        fragmentTransaction.commit()
+        btnAddAndroid = findViewById(R.id.buttonAddAndroid)
+        btnAddIOS = findViewById(R.id.buttonAddios)
+        btnReplaceAndroid = findViewById(R.id.buttonReplaceAndroid)
+        btnReplaceIOS = findViewById(R.id.buttonReplaceios)
+        btnRemoveAndroid = findViewById(R.id.buttonRemoveAndroid)
+        btnRemoveIOS = findViewById(R.id.buttonRemoveios)
+        btnPopBackStack = findViewById(R.id.buttonPopBackStack)
+        btnDetach = findViewById(R.id.buttonDetach)
+        btnAttach = findViewById(R.id.buttonAttach)
     }
 }
